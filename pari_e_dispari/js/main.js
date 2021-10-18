28e714a6f760df6ec66155383b6330d1195318c5
Bravehearts19 <leo18111998@gmail.com>
2021-10-18 16:57:36 +0200

    Pari e dispari completato

--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,22 +1,22 @@
-let isPari = document.getElementById("user_choice").value;
-let numeroSceltoDallUtente = document.getElementById("user_number").value;
-
 function generaNumeroRandomPerComputer() {
-    const numeroGeneratoPerComputer = Math.ceil(Math.random() * 5);
+    const numeroGeneratoPerComputer = Math.floor(Math.random() * 6);
     return numeroGeneratoPerComputer;
 }
 
-isPari = true;
-numeroSceltoDallUtente = 3;
-const numeroComputer = generaNumeroRandomPerComputer();
-const somma = numeroComputer + numeroSceltoDallUtente;
+document.getElementById("play").addEventListener("click", function() {
+    const isPari = document.getElementById("user_choice").value;
+    const numeroSceltoDallUtente = parseInt(document.getElementById("user_number").value);
 
-if ((somma % 2 === 0 && isPari) || (somma % 2 !== 0 && !isPari)) {
-    alert("Il numero scelto dal computer è " + numeroComputer)
-    alert("La somma è " + somma)
-    alert("Hai vinto! =)")
-} else {
-    alert("Il numero scelto dal computer è " + numeroComputer)
-    alert("La somma è " + somma)
-    alert("Hai perso! :(")
-}
+    const numeroComputer = generaNumeroRandomPerComputer();
+    const somma = numeroComputer + numeroSceltoDallUtente;
+
+    if ((somma % 2 === 0 && isPari === "pari") || (somma % 2 !== 0 && isPari === "dispari")) {
+        alert("Il numero scelto dal computer è " + numeroComputer)
+        alert("La somma è " + somma)
+        alert("Hai vinto! =)")
+    } else {
+        alert("Il numero scelto dal computer è " + numeroComputer)
+        alert("La somma è " + somma)
+        alert("Hai perso! :(")
+    }
+})
