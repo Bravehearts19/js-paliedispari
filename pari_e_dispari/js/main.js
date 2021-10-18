2786d5c8f42cdac84018c7ec1f3c0ab73f74a777
Bravehearts19 <leo18111998@gmail.com>
2021-10-18 16:20:40 +0200

    Test funzionamento del codice js

--- pari_e_dispari/js/main.js
+++ pari_e_dispari/js/main.js
@@ -1,19 +1,22 @@
-const isPari = document.getElementById("user_choice").value;
-const numeroSceltoDallUtente = document.getElementById("user_number").value;
+let isPari = document.getElementById("user_choice").value;
+let numeroSceltoDallUtente = document.getElementById("user_number").value;
 
 function generaNumeroRandomPerComputer() {
-    Math.ceil(Math.random() * 5);
+    const numeroGeneratoPerComputer = Math.ceil(Math.random() * 5);
+    return numeroGeneratoPerComputer;
 }
 
+isPari = true;
+numeroSceltoDallUtente = 3;
 const numeroComputer = generaNumeroRandomPerComputer();
 const somma = numeroComputer + numeroSceltoDallUtente;
 
 if ((somma % 2 === 0 && isPari) || (somma % 2 !== 0 && !isPari)) {
-    alert("Il numero scelto dal computer è" + numeroComputer)
-    alert("La somma è" + somma)
+    alert("Il numero scelto dal computer è " + numeroComputer)
+    alert("La somma è " + somma)
     alert("Hai vinto! =)")
 } else {
-    alert("Il numero scelto dal computer è" + numeroComputer)
-    alert("La somma è" + somma)
+    alert("Il numero scelto dal computer è " + numeroComputer)
+    alert("La somma è " + somma)
     alert("Hai perso! :(")
 }
